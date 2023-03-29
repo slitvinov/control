31da8d5b9f48a6422fa739b41f7fa0ccf3fc284d
Sergey Litvinov <slitvinov@gmail.com>
2023-03-29 18:11:59 -0400

    up

--- 25.py
+++ 25.py
@@ -7,13 +7,14 @@ def La(al):
     return [400 * al[0] * (al[1] - al[0]**2) + 2 * (1 - al[0]),
             -200 * (al[1] - al[0]**2)]
 
-al = [-0.1, 0.1]
-ep = 0.12207031e-02
+al = (1.2, 1.0)
 i = 0
 J = L(al)
 while True:
+    ep = 1
     while True:
-        alp = [al + ep * L1 for al, L1 in zip(al, La(al))]
+        L1 = La(al)
+        alp = tuple(al + ep * L1 for al, L1 in zip(al, L1))
         Jk = L(alp)
         if Jk >= J:
             J = Jk
@@ -22,8 +23,7 @@ while True:
         else:
             ep /= 2
     i += 1
-    if i in (1, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100):
-        print("%3d [%16.7e %16.7e] %.7e %.7e" % (i, *al, -J, ep))
-    ep = 1
-    if i == 100:
+    if i in (1, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 1000):
+        print("%5d [%16.7e %16.7e] %.7e %.7e" % (i, *al, -J, ep))
+    if i == 1000:
         break
